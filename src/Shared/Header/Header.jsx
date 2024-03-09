ded0a99e964ff4b46d4ebfb2aa6d2b210c6cf166
RAKIBUL ISLAM <rakibul8226@gmail.com>
2024-03-10 01:57:08 +0600

    set logo to home route

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="text-center mt-14">
-      <img className="mx-auto" src="../../../assets/logo.png" alt="" />
+      <Link to="/">
+        <img className="mx-auto" src="../../../assets/logo.png" alt="" />
+      </Link>
       <p className="mt-3 mb-2">Journalism Without Fear or Favour</p>
       <p className="text-xl mb-1">{moment().format("dddd, MMMM D, YYYY")}</p>
     </div>
